15828b9f56e4d35a6c1ffce4f70b98e4ad0d5e21
Murat Karagöz <mrt.kara.1767@gmail.com>
2021-03-07 22:16:48 +0300

    meyin

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IKrediManager tasitKrediManager = new TasitKrediManager();
+			IKrediManager konutKrediManager = new KonutKrediManager();		
+			IKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+			IKrediManager esnafKrediManager = new EsnafKrediManager();
+
+			ILoggerService dataBaseLoggerService = new DataBaseLoggerService();
+			ILoggerService fileBaseLoggerService = new FileBaseLoggerService();
+			ILoggerService smsLoggerService = new SmsLoggerService();
+
+
+			BasvuruManager basvuruManager = new BasvuruManager();
+			basvuruManager.BasvuruYap(new TasitKrediManager(),new List<ILoggerService>() {new FileBaseLoggerService()
+				,new SmsLoggerService() }) ;
+			List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,konutKrediManager,tasitKrediManager };
+			//basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+			
+		}
+	}
+}
